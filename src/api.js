865bb43c125e80fc1ebaa70bda3461a5fc4d8b72
오아람 <oaram2@uangel.com>
2022-11-25 17:59:11 +0900

    [Modify] 간단한 RESTful 서버 리팩토링

--- src/api.js
+++ src/api.js
@@ -1,47 +1,78 @@
 // @ts - check
 
 /**
+ * @typedef Post
+ * @property {string} id
+ * @property {string} title
+ * @property {string} content
+ */
+/** @type {Post[]} */
+const posts = [
+    {
+        id: "my_first_post",
+        title: "My frist post",
+        content: "Hello",
+    },
+    {
+        id: "second_post",
+        title: "first post",
+        content: "second post!",
+    },
+];
+
+/**
  * @typedef APIResponse
  * @property {number} statusCode
- * @property {*} body
+ * @property {string | object} body
  */
 
 /**
  * @typedef Route
  * @property {RegExp} url
  * @property {'GET' | 'POST'} method
- * @property {() => Promise<APIResponse>} callback
+ * @property {(matches: string[], body: Object | undefined) => Promise<APIResponse>} callback
  */
 
 /**@type {Route[]} */
 const routes = [
     {
         url: /^\/posts$/,
-        mothod: "GET",
+        method: "GET",
         callback: async () => ({
             statusCode: 200,
-            body: {},
+            body: posts,
         }),
     },
-
     {
         url: /^\/posts\/([a-zA-Z0-9-_]+)$/,
-        mothod: "GET",
-        callback: async () => ({
-            statusCode: 200,
-            body: {},
-        }),
+        method: "GET",
+        callback: async (matches) => {
+            const postId = matches[1];
+            if (!postId) {
+                return { statusCode: 404, body: "Not Found" };
+            }
+
+            const post = posts.find((_post) => _post.id === postId);
+
+            if (!post) {
+                return { statusCode: 404, body: "Not Found" };
+            }
+            return {
+                statusCode: 200,
+                body: post,
+            };
+        },
     },
     {
         url: /^\/posts$/,
         method: "POST",
-        callback: async () => ({
-            statusCode: 200,
-            body: {},
-        }),
+        callback: async (_, body) => {
+            console.log(body);
+            return { statusCode: 200, body: "test" };
+        },
     },
 ];
 
 module.exports = {
-    reoutes,
+    routes,
 };
--- src/main.js
+++ src/main.js
@@ -7,87 +7,52 @@
  * - RESTful API를 사용한다
  */
 
+const { reject } = require("core-js/fn/promise");
 const http = require("http");
-/**
- * @typedef Post
- * @property {string} id
- * @property {string} title
- * @property {string} content
- */
-/** @type {Post[]} */
-const posts = [
-    {
-        id: "my_first_post",
-        title: "My frist post",
-        content: "Hello",
-    },
-    {
-        id: "second_post",
-        title: "first post",
-        content: "second post!",
-    },
-];
-/**
- * Post
- *
- * GET /posts
- * GET /posts/:id
- * POST /posts
- */
+const { routes } = require("./api");
+
 const server = http.createServer((req, res) => {
-    const POSTS_ID_REGEX = /^\/posts\/([a-zA-Z0-9-_]+)$/;
-    const postIdRegexResult = (req.url && POSTS_ID_REGEX.exec(req.url)) || undefined;
+    async function main() {
+        const route = routes.find((_route) => req.url && req.method && _route.url.test(req.url) && _route.method === req.method);
 
-    if (req.url === "/posts" && req.method === "GET") {
-        const result = {
-            posts: posts.map((post) => ({
-                id: post.id,
-                title: post.title,
-            })),
-
-            totalCount: posts.length,
-        };
-        res.statusCode = 200;
-        res.setHeader("content-Type", "application.json", "encoding=utf-8");
-        res.end(JSON.stringify(result));
-    } else if (postIdRegexResult && req.method === "GET") {
-        // GET // posts/:id
-        const postId = postIdRegexResult[1];
-        const post = posts.find((_post) => _post.id === postId);
-
-        if (post) {
-            res.statusCode = 200;
-            res.setHeader("Content-Type", "application/json; charset=utf-8");
-            res.end(JSON.stringify(post));
-        } else {
+        if (!route && !req.url) {
             res.statusCode = 404;
-            res.end("Reading post");
+            res.end("Not Found");
+            return;
         }
-        res.statusCode = 200;
-        res.end("Some content if the post");
-    } else if (req.url === "/posts" && req.method === "POST") {
-        req.on("data", (data) => {
-            /**
-             * @typedef CreatePostBody
-             * @property {sting} title
-             * @property {string} content
-             */
-            /** @type {CreatePostBody} */
-            const body = JSON.parse(data);
-            console.log(data);
-            posts.push({
-                id: body.title.toLowerCase().replace(/\s/g, ""),
-                title: body.title,
-                content: body.content,
-            });
-        });
 
-        res.statusCode = 200;
-        res.end("Creating post");
-    } else {
-        res.statusCode = 200;
-        res.end("Not found");
+        const regexResult = route.url.exec(req.url);
+        if (!regexResult) {
+            res.statusCode = 404;
+            res.end("Not Found");
+            return;
+        }
+
+        /** @type {Object.<string, *> | undefined} */
+        const reqbody =
+            (req.headers["content-type"] === "application/json" &&
+                (await new Promise((resolve, reject) => {
+                    req.setEncoding("utf-8");
+                    req.on("data", (data) => {
+                        try {
+                            resolve(JSON.parse(data));
+                        } catch {
+                            reject(new Error("Ill-formed json"));
+                        }
+                    });
+                }))) ||
+            undefined;
+
+        const result = await route.callback(regexResult, reqBody);
+        res.statusCode = result.statusCode;
+        if (typeof result.body === "string") {
+            res.end(result.body);
+        } else {
+            res.setHeader("Content_Type", "application/json; charset-utf-8");
+            res.end(JSON.stringify(result.body));
+        }
     }
+    main();
 });
 
 const PORT = 4000;
